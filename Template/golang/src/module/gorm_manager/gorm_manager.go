7c69b6d39e30cc4bd8e7d5a95d5b6953ac56c07b
acrophobiacatwanttofly <acrophobiacatwanttofly@gmail.com>
2024-11-22 15:30:32 +0800

    feat: gorm process return error

--- Template/golang/src/module/gorm_manager/gorm_manager.go
+++ Template/golang/src/module/gorm_manager/gorm_manager.go
@@ -41,48 +41,84 @@ func (gm *Gorm_Manager) Init(dsn string, models ...interface{}){
 	
 }
 
-func (gm *Gorm_Manager) Sellect_All(all_data interface{}, conds ...interface{}){
+func (gm *Gorm_Manager) Sellect_All(all_data interface{}, conds ...interface{}) error{
 	result := gm.db.Find(all_data, conds...)
 	if result.Error != nil {
 		public.DBG_ERR("Error:", result.Error)
 	}
+	return result.Error
 }
 
-func (gm *Gorm_Manager) Create(new_item interface{}){
+func (gm *Gorm_Manager) Create(new_item interface{}) error{
 	//user := User{Name: "John Doe", Email: "john@example.com", Password: "secret"}
 
 	result := gm.db.Create(new_item)
 	if result.Error != nil {
 		public.DBG_ERR("Error:", result.Error)
 	}
+	return result.Error
 }
 
-func (gm *Gorm_Manager) Fetch(fetched_data interface{}, key interface{}){
-    gm.db.First(fetched_data, key)
+func (gm *Gorm_Manager) Fetch(fetched_data interface{}, key interface{}) error{
+    result := gm.db.First(fetched_data, key)
+    if result.Error != nil {
+		public.DBG_ERR("Error:", result.Error)
+	}
+	return result.Error
 }
 
-func (gm *Gorm_Manager) Foreign(fetched_data interface{}, key interface{}, foreign_volume string){
-	gm.db.Preload(foreign_volume).First(fetched_data, key)
+func (gm *Gorm_Manager) Foreign(fetched_data interface{}, key interface{}, foreign_volume string) error{
+	result := gm.db.Preload(foreign_volume).First(fetched_data, key)
+
+	if result.Error != nil {
+		public.DBG_ERR("Error:", result.Error)
+	}
+	return result.Error
 }
 
-func (gm *Gorm_Manager) Fetch_Where(fetched_data interface{}, where_data interface{}){
-    gm.db.Where(where_data).Find(fetched_data)
+func (gm *Gorm_Manager) Fetch_Where(fetched_data interface{}, where_data interface{}) error{
+    result := gm.db.Where(where_data).Find(fetched_data)
+
+    if result.Error != nil {
+		public.DBG_ERR("Error:", result.Error)
+	}
+	return result.Error
 }
 
-func (gm *Gorm_Manager) Foreign_Where(fetched_data interface{}, where_data interface{}, foreign_volume string){
-	gm.db.Preload(foreign_volume).Where(where_data).Find(fetched_data)
+func (gm *Gorm_Manager) Foreign_Where(fetched_data interface{}, where_data interface{}, foreign_volume string) error{
+	result := gm.db.Preload(foreign_volume).Where(where_data).Find(fetched_data)
+
+	if result.Error != nil {
+		public.DBG_ERR("Error:", result.Error)
+	}
+	return result.Error
 }
 
-func (gm *Gorm_Manager) Update(data interface{}, volume string, new_data interface{}){
-    gm.db.Model(data).Update(volume, new_data)
+func (gm *Gorm_Manager) Update(data interface{}, volume string, new_data interface{}) error{
+    result := gm.db.Model(data).Update(volume, new_data)
+
+    if result.Error != nil {
+		public.DBG_ERR("Error:", result.Error)
+	}
+	return result.Error
 }
 
-func (gm *Gorm_Manager) Updates(data interface{}, new_data interface{}){
-    gm.db.Model(data).Updates(new_data)
+func (gm *Gorm_Manager) Updates(data interface{}, new_data interface{}) error{
+    result := gm.db.Model(data).Updates(new_data)
+
+    if result.Error != nil {
+		public.DBG_ERR("Error:", result.Error)
+	}
+	return result.Error
 }
 
-func (gm *Gorm_Manager) Delete(data interface{}){
-    gm.db.Delete(data)
+func (gm *Gorm_Manager) Delete(data interface{}) error{
+    result := gm.db.Delete(data)
+
+    if result.Error != nil {
+		public.DBG_ERR("Error:", result.Error)
+	}
+	return result.Error
 }
 
 
@@ -93,40 +129,40 @@ func Init_Gorm(dsn string, v ...interface{}){
 	gorm_manager.Init(dsn, v...)
 }
 
-func Gorm_Sellect_All(all_data interface{}, conds ...interface{}){
-	gorm_manager.Sellect_All(all_data, conds...)
+func Gorm_Sellect_All(all_data interface{}, conds ...interface{}) error{
+	return gorm_manager.Sellect_All(all_data, conds...)
 }
 
-func Gorm_Create(new_item interface{}){
-	gorm_manager.Create(new_item)
+func Gorm_Create(new_item interface{}) error{
+	return gorm_manager.Create(new_item)
 }
 
-func Gorm_Fetch(fetched_data interface{}, key interface{}){
-	gorm_manager.Fetch(fetched_data, key)
+func Gorm_Fetch(fetched_data interface{}, key interface{}) error{
+	return gorm_manager.Fetch(fetched_data, key)
 }
 
-func Gorm_Foreign(fetched_data interface{}, key interface{}, foreign_volume string){
-	gorm_manager.Foreign(fetched_data, key, foreign_volume)
+func Gorm_Foreign(fetched_data interface{}, key interface{}, foreign_volume string) error{
+	return gorm_manager.Foreign(fetched_data, key, foreign_volume)
 }
 
-func Gorm_Fetch_Where(fetched_data interface{}, where_data interface{}){
-	gorm_manager.Fetch_Where(fetched_data, where_data)
+func Gorm_Fetch_Where(fetched_data interface{}, where_data interface{}) error{
+	return gorm_manager.Fetch_Where(fetched_data, where_data)
 }
 
-func Gorm_Foreign_Where(fetched_data interface{}, where_data interface{}, foreign_volume string){
-	gorm_manager.Foreign_Where(fetched_data, where_data, foreign_volume)
+func Gorm_Foreign_Where(fetched_data interface{}, where_data interface{}, foreign_volume string) error{
+	return gorm_manager.Foreign_Where(fetched_data, where_data, foreign_volume)
 }
 
-func Gorm_Update(data interface{}, volume string, new_data interface{}){
-	gorm_manager.Update(data, volume, new_data)
+func Gorm_Update(data interface{}, volume string, new_data interface{}) error{
+	return gorm_manager.Update(data, volume, new_data)
 }
 
-func Gorm_Updates(data interface{}, new_data interface{}){
-    gorm_manager.Updates(data, new_data)
+func Gorm_Updates(data interface{}, new_data interface{}) error{
+    return gorm_manager.Updates(data, new_data)
 }
 
-func Gorm_Delete(data interface{}){
-	gorm_manager.Delete(data)
+func Gorm_Delete(data interface{}) error{
+	return gorm_manager.Delete(data)
 }
 
 
