e080b87f24819aeb4ad07b02cce46597bb046eda
acrophobiacatwanttofly <acrophobiacatwanttofly@gmail.com>
2025-08-20 11:00:43 +0800

    feat: update redis.

--- Template/golang/src/module/redis_manager/redis_manager.go
+++ Template/golang/src/module/redis_manager/redis_manager.go
@@ -185,37 +185,6 @@ func (rm *Redis_Manager) List_Range(redis_key string, start_pos int64, end_pos i
     return values, true
 }
 
-func (rm *Redis_Manager) Timer_Count(redis_key string, reload_count int64, reset_time_s int64)(int64){
-	ok, err := rm.rdb.SetNX(rm.ctx, redis_key, reload_count - 1, time.Duration(reset_time_s * 1000 * 1000 * 1000)).Result()
-    if err != nil {
-    	public.DBG_ERR("Timer_Count err[", err, "]")
-        return -1
-    }
-
-    if ok {
-        // first call init succ.
-        return reload_count - 1
-    }
-
-    count, err := rm.rdb.Decr(rm.ctx, redis_key).Result()
-    if err != nil {
-        public.DBG_ERR("Timer_Count err[", err, "]")
-        return -1
-    }
-
-    if count < 0 {
-        // count done
-        ttl, err := rm.rdb.TTL(rm.ctx, redis_key).Result()
-	    if err == nil && ttl < 0 {
-	        rm.rdb.Del(rm.ctx, redis_key)
-	        return reload_count
-	    }
-        return -1
-    }
-
-    return count
-}
-
 func (rm *Redis_Manager) Add_Num(redis_key string, num int64, timeout_s ...int64)(int64, bool){
 	values, err := rm.rdb.IncrBy(rm.ctx, redis_key, num).Result()
     if err != nil {
@@ -303,10 +272,6 @@ func Stack_Get(redis_key string)(string, bool){
 
 func List_Range(redis_key string, start_pos int64, end_pos int64)([]string, bool){
 	return redis_manager.List_Range(redis_key, start_pos, end_pos)
-}
-
-func Timer_Count(redis_key string, reload_count int64, reset_time int64)(int64){
-	return redis_manager.Timer_Count(redis_key, reload_count, reset_time)
 }
 
 func Add_Num(key string, num int64, timeout_s ...int64) (int64, bool){
---
+++ Template/golang/src/module/redis_manager/timer_count.go
@@ -0,0 +1,40 @@
+package redis_manager
+
+/*
+	ret >= 0 is true
+*/
+
+func (rm *Redis_Manager) Timer_Count(redis_key string, reload_count int64, reset_time_s int64)(int64){
+	ok, err := rm.rdb.SetNX(rm.ctx, redis_key, reload_count - 1, time.Duration(reset_time_s * 1000 * 1000 * 1000)).Result()
+    if err != nil {
+    	public.DBG_ERR("Timer_Count err[", err, "]")
+        return -1
+    }
+
+    if ok {
+        // first call init succ.
+        return reload_count - 1
+    }
+
+    count, err := rm.rdb.Decr(rm.ctx, redis_key).Result()
+    if err != nil {
+        public.DBG_ERR("Timer_Count err[", err, "]")
+        return -1
+    }
+
+    if count < 0 {
+        // count done
+        ttl, err := rm.rdb.TTL(rm.ctx, redis_key).Result()
+	    if err == nil && ttl < 0 {
+	        rm.rdb.Del(rm.ctx, redis_key)
+	        return reload_count
+	    }
+        return -1
+    }
+
+    return count
+}
+
+func Timer_Count(redis_key string, reload_count int64, reset_time int64)(int64){
+	return redis_manager.Timer_Count(redis_key, reload_count, reset_time)
+}
